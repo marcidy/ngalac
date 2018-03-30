fc4683ad888b750bf1b0da926c4f687ef087fa32
Matt Arcidy <marcidy@gmail.com>
2018-03-29 21:41:45 -0700

    button, light, and obs control flow working in quotes

--- src/arduino_controller.py
+++ src/arduino_controller.py
@@ -1,37 +1,54 @@
-import PyCmdMessenger                                                           
-                                                                                
-arduino = PyCmdMessenger.ArduinoBoard("/dev/ttyUSB0", baud_rate=9600)           
-                                                                                
-commands = [['ping', ''],                                                       
-            ['pong', 's'],                                                      
-            ['player', 'i'],                                                    
-            ['lights', ''],                                                     
-            ['get_state', ''],                                                  
-            ['ret_state', 'i*'],                                                
-            ['error', '']]                                                      
-                                                                                
-c = PyCmdMessenger.CmdMessenger(arduino, commands)                              
-                                                                                
-c.send('ping')                                                                  
-msg = c.receive()                                                               
-print(msg)                                                                      
-                                                                                
-c.send('player')                                                                
-print(c.receive())                                                              
-                                                                                
-#  Turn On Lights                                                               
-c.send('lights')                                                                
-                                                                                
-c.send('get_state')                                                             
-msg = c.receive()                                                               
-print(msg)                                                                      
-                                                                                
-#  Turn off Lights                                                              
-c.send('lights')                                                                
-c.send('get_state')                                                             
-msg = c.receive()                                                               
-print(msg)                                                                      
-                                                                                
-controls = msg[1]                                                               
-if controls[0] != 0:                                                            
-    print("Start Stream!")
+import PyCmdMessenger
+import obswsrc
+
+COMMANDS = [['ping', ''],
+        ['pong', 's'],
+        ['player', 'i'],
+        ['lights', 's'],
+        ['get_state', ''],
+        ['ret_state', 'i*'],
+        ['release_latches', 's'],
+        ['error', '']]
+
+
+class NgalacArduinoController():
+    '''  NGALAC uses an arduino to control button presses and turn on various lights.
+    '''
+
+    def __init__(self):
+        self.arduino = PyCmdMessenger.ArduinoBoard("COM5", baud_rate=9600)
+        self.commands = dict(COMMANDS)
+        self.c = PyCmdMessenger.CmdMessenger(self.arduino, COMMANDS)
+        self.arduino_state = self.get_state()
+
+    def _send_cmd(self, cmd):
+        if cmd in self.commands:
+            self.c.send(cmd)
+            return self._parse_msg(self.c.receive())
+        else:
+            pass  # heh, logging pls thx
+
+    def ping(self):
+        return self._send_cmd('ping')
+        
+    def is_player(self):
+        return self._send_cmd('player')
+
+    def flip_lights(self):
+        return self._send_cmd('lights')
+
+    def get_state(self):
+        return self._send_cmd('get_state')
+
+    def _parse_msg(self, msg):
+        self.last_cmd, value, self.exec_time = msg
+        return value
+
+    def release_latches(self):
+        return self._send_cmd('release_latches')
+
+    def open(self):
+        self.arduino.open()
+
+    def close(self):
+        self.arduino.close()
--- src/pycontroller.py
+++ src/pycontroller.py
@@ -1,13 +1,26 @@
 import asyncio
 from obswsrc import OBSWS
-from obswsrc.requests import GetStreamingStatusRequest
+from obswsrc.requests import (GetStreamingStatusRequest, 
+        StartStreamingRequest, 
+        StopStreamingRequest,
+        StartStopStreamingRequest
+        )
 from obswsrc.types import Stream, StreamSettings
+from arduino_controller import NgalacArduinoController as acontrol
+
 
 async def main():
+    g = acontrol()
+    streaming = False
+
     async with OBSWS('localhost', 4444, 'password') as obsws:
         response = await obsws.require(GetStreamingStatusRequest())
-        print("Streaming: {}".format(response.streaming))
-        print("Recording: {}".format(response.recording))
+        if g.get_state()[11] == 1:
+            g.flip_lights()
+            g.release_latches()
+
+            await obsws.require(StartStopStreamingRequest())
+
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
